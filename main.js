9db23a5cc3959b925d3ddd8729bef303a32959e6
Daniel Cudney <upnvapesd@gmail.com>
2016-07-26 13:54:05 -0700

    Changed Index, Added Main.js
    Changed the CTA and added JavaScript to the unlock your potential
    button.

---
+++ main.js
@@ -0,0 +1,20 @@
+var lockToggle = function() {
+  if (($"#PADLOCK").hasClass("fa-lock")) {
+    $("#PADLOCK").addclass("fa-unlock-alt");
+    $("#PADLOCK").removeclass("fa-lock");
+  }
+  if else (($"#PADLOCK").hasClass("fa-unlock-alt")) {
+    $("#PADLOCK").addclass("fa-unlock");
+    $("#PADLOCK").removeclass("fa-unlock-alt");
+  }
+  if else (($"#PADLOCK").hasClass("fa-unlock")) {
+    $("#PADLOCK").addclass("fa-lock");
+    $("#PADLOCK").removeclass("fa-unlock");
+  }
+}
+
+
+
+$(document).ready(function() {
+  $("#PADLOCKBTN").on("click", lockToggle())
+})
